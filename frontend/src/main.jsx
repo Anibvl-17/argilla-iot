81d55691ff066e48754131a0823fc56d3ee5cb2d
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-06-15 23:45:33 -0400

    feat: rutas protegidas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoute from "@components/ProtectedRoute";
 import ReactDOM from "react-dom/client";
 import Login from "@pages/Login";
 import Root from "@pages/Root";
 import Home from "@pages/Home";
-import "./index.css"
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -21,12 +22,16 @@ const router = createBrowserRouter([
       },
       {
         path: "/home",
-        element: <Home />,
-      }
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-)
+  <RouterProvider router={router} />,
+);
