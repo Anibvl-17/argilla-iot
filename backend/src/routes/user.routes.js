fba9cadf8f30e6459becf0a4746944584711b249
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-06-21 02:09:43 -0400

    refactor: agrega campo rol, distingue entre registrar/crear

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,12 +3,15 @@ import { authenticateJWT } from "../middlewares/authentication.middleware.js";
 import { verifyRoles } from "../middlewares/authorization.middleware.js";
 import { ROLES } from "../constants/roles.constants.js";
 import {
+  addUser,
   editProfile,
   editUser,
+  getAllUsers,
   removeUser,
 } from "../controllers/user.controller.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import {
+  createUserValidation,
   updateProfileValidation,
   updateUserValidation,
 } from "../validations/user.validation.js";
@@ -21,6 +24,8 @@ router.patch("/me", validateSchema(updateProfileValidation), editProfile);
 
 router.use(verifyRoles([ROLES.ADMIN]));
 
+router.get("/all", getAllUsers);
+router.post("/create", validateSchema(createUserValidation), addUser);
 router.patch("/:userId", validateSchema(updateUserValidation), editUser);
 router.delete("/:userId", removeUser);
 
