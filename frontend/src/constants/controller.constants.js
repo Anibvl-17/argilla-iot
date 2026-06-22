92aea58088065667a7500c0b05f41dfc0e72361f
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-06-22 01:07:09 -0400

    feat: constantes

---
+++ frontend/src/constants/controller.constants.js
@@ -0,0 +1,16 @@
+export const CONTROLLER_LINK_STATUS = {
+  UNLINKED: "UNLINKED",
+  LINKED_TO_KILN: "LINKED_TO_KILN",
+  LINKED_TO_KILN_AND_USER: "LINKED_TO_KILN_AND_USER",
+};
+
+export const CONTROLLER_LINK_STATUS_LABELS = {
+  UNLINKED: "No enlazado",
+  LINKED_TO_KILN: "Enlazado a Horno",
+  LINKED_TO_KILN_AND_USER: "Enlazado a Horno y Usuario",
+};
+
+export const SWITCH_LABELS = {
+  CONTACTOR: "Contactor",
+  SSR: "SSR"
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
       "@services": path.resolve(__dirname, "./src/services"),
       "@context": path.resolve(__dirname, "./src/context"),
       "@helpers": path.resolve(__dirname, "./src/helpers"),
